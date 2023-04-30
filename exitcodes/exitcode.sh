583c59875facdbee63d6506b7acb909107d19012
scriptkiddo <dasbiradar1999@gmail.com>
2023-05-01 00:25:50 +0530

    added files for ext codes

---
+++ exitcodes/exitcode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "My name is Tulasidas"
+
+echo "The exit code is $?"
+cat hello.txt >> failure_log.log
+
+echo "The vaule of exit code is $?"
---
+++ exitcodes/exitcode1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "This command executed successfully"
+echo $?
+
+exit 177
